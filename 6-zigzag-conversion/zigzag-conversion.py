d6d8d4d7f310e69b43ffccef176c123d592259e0
Maria Tesfaye <mariatesfaye67@gmail.com>
2025-07-29 17:02:45 +0300

    Time: 9 ms (86.62%) | Memory: 12.5 MB (55.12%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        rows = [''] * numRows
+        current_row = 0
+        step = 1 
+
+        for char in s:
+            rows[current_row] += char
+            if current_row == 0:
+                step = 1
+            elif current_row == numRows - 1:
+                step = -1
+            current_row += step
+
+        return ''.join(rows)
