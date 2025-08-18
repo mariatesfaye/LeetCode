a85cfb45e6c9962804e202195a2457b08c0da512
Maria Tesfaye <mariatesfaye67@gmail.com>
2025-08-18 10:30:39 +0300

    Time: 3 ms (52.83%) | Memory: 12.4 MB (84.34%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        
+        brackets = {')': '(', '}': '{', ']': '['}
+        
+        for char in s:
+            if char in brackets.values():
+               
+                stack.append(char)
+            elif char in brackets:
+              
+                if not stack or stack.pop() != brackets[char]:
+                    return False
+            else:
+ 
+                return False
+
+        return len(stack) == 0
