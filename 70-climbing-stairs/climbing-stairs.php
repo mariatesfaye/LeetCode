74300cc707c6e9b8c57afa214e7e7f5a93d5be6f
Maria Tesfaye <mariatesfaye67@gmail.com>
2025-08-22 14:14:51 +0300

    Time: 0 ms (100.00%) | Memory: 20.5 MB (60.28%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.php
@@ -0,0 +1,21 @@
+class Solution {
+
+    /**
+     * @param Integer $n
+     * @return Integer
+     */
+    function climbStairs($n) {
+        if ($n <= 2) return $n;
+
+        $first = 1; 
+        $second = 2; 
+
+        for ($i = 3; $i <= $n; $i++) {
+            $third = $first + $second;
+            $first = $second;
+            $second = $third;
+        }
+
+        return $second;
+    }
+}
