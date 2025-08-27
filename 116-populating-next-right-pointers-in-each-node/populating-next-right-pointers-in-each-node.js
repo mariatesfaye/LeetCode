c0788e7e7650ccb89e5a56e3b06397fde94c8bb2
Maria Tesfaye <mariatesfaye67@gmail.com>
2025-08-27 16:46:48 +0300

    Time: 57 ms (47.90%) | Memory: 61.1 MB (68.74%) - LeetSync

---
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.js
@@ -0,0 +1,28 @@
+/**
+ * // Definition for a Node.
+ * function _Node(val, left, right, next) {
+ *     this.val = val === undefined ? null : val;
+ *     this.left = left === undefined ? null : left;
+ *     this.right = right === undefined ? null : right;
+ *     this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {_Node} root
+ * @return {_Node}
+ */
+var connect = function(root) {
+    if (!root || !root.left) return root;
+
+    root.left.next = root.right;
+
+    if (root.next) {
+        root.right.next = root.next.left;
+    }
+
+    connect(root.left);
+    connect(root.right);
+
+    return root;
+};
