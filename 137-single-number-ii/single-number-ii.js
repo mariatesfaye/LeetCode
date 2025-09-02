25a942daab4601706344656806ab6f1ba0b71550
Maria Tesfaye <mariatesfaye67@gmail.com>
2025-09-02 10:55:24 +0300

    Time: 1 ms (95.70%) | Memory: 55.1 MB (65.23%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.js
@@ -0,0 +1,10 @@
+var singleNumber = function(nums) {
+    let ones = 0, twos = 0;
+
+    for (let num of nums) {
+        ones = (ones ^ num) & ~twos;
+        twos = (twos ^ num) & ~ones;
+    }
+
+    return ones;
+};
