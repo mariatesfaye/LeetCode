131f3098193b5a367f523bb081f3d5f11c45fe7f
Maria Tesfaye <mariatesfaye67@gmail.com>
2025-09-02 11:00:00 +0300

    Time: 54 ms (48.18%) | Memory: 59.2 MB (48.21%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.js
@@ -0,0 +1,14 @@
+var hasCycle = function(head) {
+    if (!head || !head.next) return false;
+    
+    let slow = head;
+    let fast = head.next;
+    
+    while (fast && fast.next) {
+        if (slow === fast) return true;
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    
+    return false;
+};
