a933ddab4d7f9c5ae32ad188d558b3c2413ea338
Maria Tesfaye <mariatesfaye67@gmail.com>
2025-09-05 11:54:27 +0300

    Time: 282 ms (45.19%) | Memory: 29 MB (41.82%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -5,21 +5,18 @@ class Solution:
         if not nums or k == 0:
             return []
 
-        dq = deque()  # will store indices
+        dq = deque() 
         result = []
 
         for i in range(len(nums)):
-            # Remove indices out of the window
             while dq and dq[0] <= i - k:
                 dq.popleft()
 
-            # Remove smaller values from the back
             while dq and nums[dq[-1]] < nums[i]:
                 dq.pop()
 
             dq.append(i)
 
-            # Starting to add max values to result once we hit the window size
             if i >= k - 1:
                 result.append(nums[dq[0]])
 
