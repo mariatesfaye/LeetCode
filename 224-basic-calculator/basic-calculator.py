2a32ed96b8757fa98d0aeb513af34d7a913ba435
Maria Tesfaye <mariatesfaye67@gmail.com>
2025-09-05 11:28:00 +0300

    Time: 61 ms (52.65%) | Memory: 13.1 MB (69.56%) - LeetSync

--- 224-basic-calculator/basic-calculator.py
+++ 224-basic-calculator/basic-calculator.py
@@ -1,9 +1,9 @@
 class Solution:
     def calculate(self, s):
         stack = []
-        result = 0  # current result
-        number = 0  # current number being built
-        sign = 1    # current sign (+1 or -1)
+        result = 0 
+        number = 0 
+        sign = 1 
         i = 0
         n = len(s)
 
@@ -21,22 +21,17 @@ class Solution:
                 number = 0
                 sign = -1
             elif char == '(':
-                # Push current result and sign on the stack for later
                 stack.append(result)
                 stack.append(sign)
-                # Reset for new sub-expression
                 result = 0
                 sign = 1
             elif char == ')':
                 result += sign * number
                 number = 0
-                # Pop sign and previous result
                 prev_sign = stack.pop()
                 prev_result = stack.pop()
                 result = prev_result + prev_sign * result
-            # skip spaces
             i += 1
 
-        # Add the last number
         result += sign * number
         return result
