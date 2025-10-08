50c2515a52fdde67e58db76055520e97b00a375e
Maria Tesfaye <mariatesfaye67@gmail.com>
2025-10-08 10:30:23 +0300

    Time: 2 ms (78.40%) | Memory: 62 MB (65.03%) - LeetSync

---
+++ 338-counting-bits/counting-bits.ts
@@ -0,0 +1,9 @@
+function countBits(n: number): number[] {
+    const ans = new Array(n + 1).fill(0);
+
+    for (let i = 1; i <= n; i++) {
+        ans[i] = ans[i >> 1] + (i & 1);
+    }
+
+    return ans;
+}
