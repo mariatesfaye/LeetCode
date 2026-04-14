3ecaef100fe56cdc538dd48d01efaf621d2fe502
Maria Tesfaye <mariatesfaye67@gmail.com>
2026-04-14 16:30:36 +0300

    Time: 0 ms (100.00%) | Memory: 12.5 MB (54.10%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,10 +1,5 @@
 class Solution(object):
     def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
         left, right = 0, len(nums) - 1
         
         while left <= right:
@@ -12,16 +7,13 @@ class Solution(object):
             
             if nums[mid] == target:
                 return mid
-
+            
             if nums[left] <= nums[mid]:
-
                 if nums[left] <= target < nums[mid]:
                     right = mid - 1
                 else:
                     left = mid + 1
-
             else:
-
                 if nums[mid] < target <= nums[right]:
                     left = mid + 1
                 else:
