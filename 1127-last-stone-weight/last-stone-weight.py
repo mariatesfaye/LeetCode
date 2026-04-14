5912c50e9ec1735874be97abb206fdc3df50e0fb
Maria Tesfaye <mariatesfaye67@gmail.com>
2026-04-14 14:21:37 +0300

    Time: 0 ms (100.00%) | Memory: 12.3 MB (63.37%) - LeetSync

---
+++ 1127-last-stone-weight/last-stone-weight.py
@@ -0,0 +1,15 @@
+import heapq
+
+class Solution(object):
+    def lastStoneWeight(self, stones):
+        heap = [-s for s in stones]
+        heapq.heapify(heap)
+        
+        while len(heap) > 1:
+            first = -heapq.heappop(heap)
+            second = -heapq.heappop(heap)
+            
+            if first != second:
+                heapq.heappush(heap, -(first - second))
+        
+        return -heap[0] if heap else 0
